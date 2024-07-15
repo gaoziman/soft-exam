450ea7b72c86eb1e87da0ca64dc0ccd983e82070
gaoziman <2942894660@qq.com>
2024-07-15 21:38:42 +0800

    添加数据库配置

---
+++ src/main/Java/org/javatop/exam/utils/DatabaseUtil.java
@@ -0,0 +1,31 @@
+package org.javatop.exam.utils;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+/**
+ * @author : Leo
+ * @version 1.0
+ * @date 2024-07-15 21:34
+ * @description : 数据库连接工具类
+ */
+public class DatabaseUtil {
+    private static final String JDBC_DRIVER = "com.mysql.cj.jdbc.Driver";
+    private static final String DB_URL = "jdbc:mysql://xxx.xxx.xxx.xxx:3306/oss213?serverTimezone=Asia/Shanghai&autoReconnect=true&useUnicode=true&characterEncoding=utf-8";
+    private static final String USER = "root";
+    private static final String PASS = "xxxxxx";
+
+    public static Connection getConnection() throws SQLException {
+        // 注册 JDBC 驱动
+        try {
+            Class.forName(JDBC_DRIVER);
+        } catch (ClassNotFoundException e) {
+            throw new SQLException("JDBC Driver not found.", e);
+        }
+
+        // 打开连接
+        Connection conn = DriverManager.getConnection(DB_URL, USER, PASS);
+        return conn;
+    }
+}
